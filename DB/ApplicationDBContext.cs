6920659f1604748ff88d486ee50b6dc12c79dc33
Modestas Apanavicius <modestas.apanavicius@gmail.com>
2023-01-14 18:35:08 +0200

    DB setup

---
+++ DB/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Final_Exam.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Final_Exam.DB
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
